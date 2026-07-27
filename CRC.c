e2e68923d5272b2f1f4a31cc93691354323c23d2
Mashuk Rahman <mashukrahman.mahir@gmail.com>
2026-07-27 11:36:01 +0600

    crc.c
    The error of this code has been fixed successfully .

--- CRC.c
+++ CRC.c
@@ -1,99 +1,87 @@
 //this is the c programme of Cyclic Redundancy Check (CRC)
 // Data com Lab 
 
-// fix error
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
+int main() {
 
-char data[50],generator[20],temp[50],remainder[20];
-int datalen,genlen,i,j;
+    char data[50], generator[20], temp[50], remainder[20];
+    char dividend[50];
+    int datalen, genlen, i, j;
 
-printf("Enter Data bits : ");
-scanf("%s",data);
+    printf("Enter Data bits : ");
+    scanf("%s", data);
 
-printf("Enter Generator bits : ");
-scanf("%s",generator);
+    printf("Enter Generator bits : ");
+    scanf("%s", generator);
 
+    datalen = strlen(data);
+    genlen = strlen(generator);
 
-datalen=strlen(data);
-genlen=strlen(generator);
+    // Copy data and append zeros
+    strcpy(temp, data);
 
-strcpy(temp,data);
+    for (i = 0; i < genlen - 1; i++) {
+        temp[datalen + i] = '0';
+    }
+    temp[datalen + genlen - 1] = '\0';
 
-//Append Zeros
+    strcpy(dividend, temp);
 
-for(i=0;i<genlen-1;i++){
-    temp[datalen+i]='0';
-}
-
-temp[datalen+genlen-1]='\0';
-
-char dividend[50];
-strcpy(dividend,temp);
-
-//CRC division
-for (i=0;i<datalen;i++){
-
-    if (dividend[i]=='1'){
-        for(j=1;j<genlen;i++){
-            dividend[i+j]=(dividend[i+j]==generator[j] ) ? '0':'1'; // that old ternary operator
-
+    // ---------------- Sender CRC Division ----------------
+    for (i = 0; i < datalen; i++) {
+        if (dividend[i] == '1') {
+            for (j = 1; j < genlen; j++) {
+                dividend[i + j] =
+                    (dividend[i + j] == generator[j]) ? '0' : '1';
+            }
         }
     }
-}
 
-//Extract Remider
-for(int i=0;i<genlen-1;i++){
-    remainder[i]=dividend[datalen+i];
-}
+    // Extract remainder
+    for (i = 0; i < genlen - 1; i++) {
+        remainder[i] = dividend[datalen + i];
+    }
+    remainder[genlen - 1] = '\0';
 
-remainder[genlen-1]='\0';
-printf("\n\n NRC Reminder : %s\n  ",remainder);
+    printf("\nCRC Remainder : %s\n", remainder);
 
+    // Generate transmitted codeword
+    strcpy(temp, data);
+    strcat(temp, remainder);
 
-//generate codeword
-strcpy(temp,data);
-strcat(temp,remainder);
+    printf("Transmitted Codeword : %s\n", temp);
 
-printf("\n Transmitted codeword : %s\n ",temp);
+    // ---------------- Receiver Side ----------------
+    printf("\nReceiver Checking...\n");
 
+    strcpy(dividend, temp);
 
-// Reciever Side
-
-printf("Reciever Checking . . . . . . . \n");
-
-strcpy(dividend,temp); // copy codeword to divedend
-
-
-for (i=0;i<datalen;i++){
-
-    if (dividend[i]=='1'){
-        for(j=1;j<genlen;i++){
-            dividend[i+j]=(dividend[i+j]==generator[j]) ? '0':'1'; // that old ternary operator
-
+    // Divide received codeword
+    for (i = 0; i < datalen; i++) {
+        if (dividend[i] == '1') {
+            for (j = 1; j < genlen; j++) {
+                dividend[i + j] =
+                    (dividend[i + j] == generator[j]) ? '0' : '1';
+            }
         }
     }
-}
 
+    // Check remainder
+    int error = 0;
 
-int error=0;
-
-for(i=datalen;i<datalen+genlen-1;i++){
-       if(dividend[i] !=0){
-            error=0;
+    for (i = datalen; i < datalen + genlen - 1; i++) {
+        if (dividend[i] != '0') {
+            error = 1;
             break;
-       }
+        }
+    }
 
-}
-
-
-if(error){
-    printf("Oh No !! Errorrrr Found ");
-}
-else
-    printf("Yay its Error freeeeee");
+    if (error)
+        printf("Oh No!! Error Found.\n");
+    else
+        printf("Yay! Error Free.\n");
 
     return 0;
 }
